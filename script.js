d2b0f66f53c0c968fa47d922c3d0526132acb9c8
Mikail-Savas <savasmikail1024@gmail.com>
2025-06-05 23:05:43 +0200

    feat(search results): add HTML for dropdown menu

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const refs = {};
 const P = new Pokedex.Pokedex();
+
+const dropdownElement = document.querySelector('.btn-dropdown');
+const dropdown = new bootstrap.Dropdown(dropdownElement);
+dropdown.show();
+
 function init() { }
