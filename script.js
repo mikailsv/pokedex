523665f8a9bbf5939065854e551a31f85956f099
Mikail-Savas <savasmikail1024@gmail.com>
2025-06-08 19:23:46 +0200

    fix: prevent image render without src and clear pokemon input

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const refs = {
     dropdownTrigger: document.querySelector('[data-bs-toggle=dropdown]'),
     dropdownMenu: document.querySelector('.dropdown-menu'),
-    pokemonModal: document.getElementById('pokemon-details-modal')
+    pokemonInput: document.getElementById('pokemon-query'),
+    pokemonModal: document.getElementById('pokemon-details-modal'),
+    pokemonImage: document.getElementById('pokemon-image'),
 };
 const P = new Pokedex.Pokedex({ cacheImages: true });
 let pokemons = [];
@@ -23,7 +25,7 @@ function search() {
 const dropdown = new bootstrap.Dropdown(refs.dropdownTrigger);
 
 function handlePokemonInput() {
-    const pokemonName = document.getElementById('pokemon-query').value.toLowerCase();
+    const pokemonName = refs.pokemonInput.value.toLowerCase();
     const suggestions = pokemons.filter(pokemon => pokemon.name.startsWith(pokemonName)).slice(0, 4);
     if (suggestions.length == 0 || pokemonName.length == 0) { dropdown.hide(); return; }
     renderPokemonSuggestions(suggestions);
@@ -37,30 +39,34 @@ function renderPokemonSuggestions(pokemonSuggestions) {
 
 if (refs.pokemonModal) {
     refs.pokemonModal.addEventListener('show.bs.modal', async event => {
+        clearPokemonInput();
         const name = event.relatedTarget.getAttribute('data-bs-pokemon');
-        const pokemon = await P.getPokemonByName(name);
-        console.log(pokemon);
-        updatePokemonModal(pokemon);
+        try {
+            const pokemon = await P.getPokemonByName(name);
+            console.log(pokemon);
+            updatePokemonModal(pokemon);
+        } catch (error) {
+            // TODO: handle error 
+        }
     })
     refs.pokemonModal.addEventListener('hide.bs.modal', async event => {
         resetPokemonModal();
     })
 }
 
-function updatePokemonModal(pokemon) {
-    const pokemonImage = refs.pokemonModal.querySelector('#pokemon-image');
-    pokemonImage.src = pokemon.sprites.other['official-artwork'].front_default;
-    pokemonImage.onload = () => {
-        handlePlaceholderClass('remove');
-    }
-
-    // const modalTitle = refs.pokemonModal.querySelector('.modal-title')
-    // const modalBody = refs.pokemonModal.querySelector('.modal-body')
-    // modalTitle.textContent = `New message to ${recipient}`
-    // modalBodyInput.value = recipient
+function clearPokemonInput() {
+    refs.pokemonInput.value = '';
 }
 
-function handlePlaceholderClass(method) {
+function updatePokemonModal(pokemon) {
+    refs.pokemonImage.src = pokemon.sprites.other['official-artwork'].front_default;
+    refs.pokemonImage.onload = () => {
+        togglePlaceholderClass('remove');
+        refs.pokemonImage.classList.remove('invisible');
+    }
+}
+
+function togglePlaceholderClass(method) {
     const placeholders = refs.pokemonModal
         .querySelector('.modal-body')
         .querySelectorAll('.placeholder-target');
@@ -68,6 +74,7 @@ function handlePlaceholderClass(method) {
 }
 
 function resetPokemonModal() {
-    refs.pokemonModal.querySelector('#pokemon-image').removeAttribute('src');
-    handlePlaceholderClass('add');
+    refs.pokemonImage.removeAttribute('src');
+    refs.pokemonImage.classList.add('invisible')
+    togglePlaceholderClass('add');
 }
