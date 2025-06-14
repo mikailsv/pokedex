c7a7495beed48056fc0603c9fa4b3ceceb018b95
Mikail-Savas <savasmikail1024@gmail.com>
2025-06-14 20:29:09 +0200

    style(pokecard-ctn): add bg-light to main and scale down pokemon image

--- assets/scripts/template.js
+++ assets/scripts/template.js
@@ -27,13 +27,15 @@ function getPokecardTemplate(pokemon) {
     return `
     <div class="col-6 col-md-4">
         <div 
-        class="card position-relative p-2" 
+        class="card position-relative p-3" 
         style="height: 100px;"
         data-bs-toggle="modal"
         data-bs-pokemon="${pokemon.name}"
         data-bs-target="#pokemon-details-modal">
-            <h6 class="card-title">${pokemon.name}</h6>
-            <img class="position-absolute top-50 end-0 translate-middle-y"
+            <h6 class="card-title text-capitalize">${pokemon.name}</h6>
+            <img 
+            style="scale:0.8"
+            class="position-absolute top-50 end-0 translate-middle-y"
             src="${pokemon.sprites.front_default}" alt="pokemon image">
         </div>
     </div>
