8c018a1ba5e64a4e65c262fcfad0ab3ac39a40d1
Mikail-Savas <savasmikail1024@gmail.com>
2025-06-14 20:58:28 +0200

    style(pokecard): add light background based on pokemon type

--- assets/scripts/template.js
+++ assets/scripts/template.js
@@ -27,12 +27,13 @@ function getPokecardTemplate(pokemon) {
     return `
     <div class="col-6 col-md-4">
         <div 
-        class="card position-relative p-3" 
+        class="card position-relative p-3 bg-${pokemon.types.map(typeWrapper => typeWrapper.type.name)[0]}-light" 
         style="height: 100px;"
         data-bs-toggle="modal"
         data-bs-pokemon="${pokemon.name}"
         data-bs-target="#pokemon-details-modal">
             <h6 class="card-title text-capitalize">${pokemon.name}</h6>
+            <span class="text-secondary">#${pokemon.id}</span>
             <img 
             style="scale:0.8"
             class="position-absolute top-50 end-0 translate-middle-y"
